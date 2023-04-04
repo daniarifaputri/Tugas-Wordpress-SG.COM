def6d14e7f386f8ea2431e268b079647f5268949
daniarifaputri <91362543+daniarifaputri@users.noreply.github.com>
2023-04-05 00:15:37 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+function Pusing (&$x) {
+if (($x % 2) == 1) {
+	
+	else {
+	$x++;
+	}
+}
+ function GaTahu ($i){
+ 	$i = $i * 2;
+ }
+
+ $i = 0;
+
+ while ( $i < 10) {
+ 	Pusing ($i);
+ 	GaTahu ($i);
+ 	echo $i;
+ 	$i++;
+ }
+
+
+ ?>
